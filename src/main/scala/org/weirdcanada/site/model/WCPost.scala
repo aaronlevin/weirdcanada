05aaadf5e5841a24e1401217c4a9a741d8b9893c
Aaron Levin <aaron@weirdcanada.com>
2013-07-10 23:24:54 -0400

    Add audio tags to admin button

--- src/main/scala/org/weirdcanada/site/model/WCPost.scala
+++ src/main/scala/org/weirdcanada/site/model/WCPost.scala
@@ -147,6 +147,12 @@ object Post {
     </div>
   </div>
 </div>
+
+<audio controls="controls" class="audioTrack" preload="none" >
+<source src="OGG_FILE_URL"  type="audio/ogg" />
+<source src="MP3_FILE_URL"  type="audio/mpeg" />
+</audio>
+<p class="audioTrack"><a href="MP3_FILE_URL" target="_blank">ARTIST_NAME - TRACK_NAME</a></p>
   }
 }
 
