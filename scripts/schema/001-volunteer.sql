e9a71f5d81fe6a3919cf2323cee6c72a9035a250
Aaron Levin <aaron@weirdcanada.com>
2013-08-13 22:58:31 -0400

    Add wc_volunteer table creation

---
+++ scripts/schema/001-volunteer.sql
@@ -0,0 +1,34 @@
+BEGIN;
+  CREATE TABLE wc_volunteer (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone VARCHAR(12) NOT NULL,
+    city TEXT NOT NULL,
+    province varchar(2) NOT NULL,
+    availability TEXT,
+    why TEXT,
+    gender VARCHAR(10),
+    address TEXT,
+    birthday TIMESTAMP,
+    bio TEXT,
+    byline TEXT,
+    website TEXT,
+    image TEXT
+  );
+
+  CREATE TABLE wc_volunteer_interest (
+    id SERIAL PRIMARY KEY,
+    interest TEXT NOT NULL,
+    volunteer_id INTEGER REFERENCES wc_volunteer(id) ON DELETE CASCADE
+  );
+
+  CREATE INDEX wc_volunteer_interest_idx ON wc_volunteer_interest (lower(interest));
+END;
+
+    
+
+
+
+  -- interests in different table
