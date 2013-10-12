981966218be768603d2c25650802672e745262a7
Aaron Levin <aaron@weirdcanada.com>
2013-10-12 15:36:45 -0400

    FreeQuery: FromQ subquery compiles

--- common/src/main/scala/org/weirdcanada/common/query/Query.scala
+++ common/src/main/scala/org/weirdcanada/common/query/Query.scala
@@ -7,7 +7,7 @@ sealed trait Query[+A]
 
 case class Select[A](columns: Iterable[String], next: A) extends Query[A]
 case class From[A](tables: Iterable[String], next: A) extends Query[A]
-case class FromQ[A,B](subQuery: Query[Free[B,A], next: A) extends Query[A]
+case class FromQ[A](subQuery: Free[Query,Unit], next: A) extends Query[A]
 case object Done extends Query[Nothing]
 
 object Query {
@@ -28,36 +28,42 @@ object Query {
   def from(tables: Iterable[String]): Free[Query, Unit] =
     Suspend(From(tables, Return(())))
 
-  def fromQ[A,B](subQuery: Query[Free[B,A]]): Free[Query, Unit] = 
+  def fromQ(subQuery: Free[Query,Unit]): Free[Query, Unit] = 
     Suspend(FromQ(subQuery, Return(())))
  
   def done: Free[Query, Unit] = 
     Return(Done)
 
-  def sqlInterpreter[A](query: Free[Query, A], string: String): String = query match {
-    case Suspend(Select(columns, a)) => sqlInterpreter(a, string + "select %s".format(columns.mkString(",")))
-    case Suspend(From(tables, a)) => sqlInterpreter(a, string + "from %s".format(tables.mkString(",")))
-    case Suspend(FromQ(subquery, a)) => 
-      sqlInterpreter(a, string + "from ( %s )".format(sqlInterpreter(Suspend(subquery),"")))
-    case Suspend(Done) => string
-    case Return(a) => string
+  def sqlInterpreter[A](query: Free[Query, A], statements: List[String]): String = query match {
+    case Suspend(Select(columns, a)) => sqlInterpreter(a, statements :::  "select %s".format(columns.mkString(",")) :: Nil)
+    case Suspend(From(tables, a)) => sqlInterpreter(a, statements ::: "from %s".format(tables.mkString(",")) :: Nil)
+    case Suspend(FromQ(subquery, a)) => sqlInterpreter(a, statements ::: "from ( %s )".format(sqlInterpreter(subquery,Nil)) :: Nil)
+    case Suspend(Done) => statements.mkString("\n")
+    case Return(a) => statements.mkString("\n")
     case Gosub(a, f) => {
       val freeA = a()
-      val value = sqlInterpreter(freeA, string)
-      freeA match {
+      val value = sqlInterpreter(freeA, statements)
+      sqlInterpreter(f(()), List(value))
+      /*freeA match {
         case Suspend(Select(columns,_)) => sqlInterpreter(f(()), value)
         case Suspend(From(tables, _)) => sqlInterpreter(f(()), value)
         case Suspend(FromQ(_,_)) => sqlInterpreter(f(()), value)
         case Return(newA) => sqlInterpreter(f(newA), value)
         case _ => value
-      }
+      }*/
     }
   }
+ 
+  val sub: Free[Query, Unit] = 
+    for {
+      _ <- select("XXXX" :: "YYY" :: Nil)
+    } yield ()
 
   val tmp: Free[Query, Unit] =
     for {
       y <- select("column1" :: "column2" :: Nil)
       _ <- from("cool" :: Nil) 
+      _ <- fromQ(sub)
       _ <- done
     } yield ()
 
@@ -69,7 +75,7 @@ object QueryMain {
 
     import Query._
 
-    println("*** tmp: %s".format(sqlInterpreter(tmp,"")))
+    println("*** tmp: %s".format(sqlInterpreter(tmp,Nil)))
 
   }
 
