fdb4dcbcf6088fe933e462427f05b6de711b3544
PhilipDavis <mrphilipadavis@gmail.com>
2013-10-13 23:34:17 -0400

    Add ability to lookup Sale by Order id

--- distro/src/main/scala/org/weirdcanada/distro/data/Sale.scala
+++ distro/src/main/scala/org/weirdcanada/distro/data/Sale.scala
@@ -8,7 +8,7 @@ import org.weirdcanada.distro.api.shopify.Order
 class Sale extends LongKeyedMapper[Sale] with IdPK with Address {
   def getSingleton = Sale
 
-  object orderId extends MappedLong(this) with DBIndexed
+  object orderId extends MappedLong(this) //with DBIndexed
   object lineItemId extends MappedLong(this)
   object dateTime extends MappedDateTime(this) with DBIndexed
   object consignedItem extends MappedLongForeignKey(this, ConsignedItem)
@@ -27,7 +27,13 @@ class Sale extends LongKeyedMapper[Sale] with IdPK with Address {
 
 // The companion object to the above Class
 object Sale extends Sale with LongKeyedMetaMapper[Sale] {
+  override def dbIndexes = UniqueIndex(orderId) :: super.dbIndexes
+  
   def getLatestOrderId = {
     DB.runQuery("SELECT MAX(orderId) FROM sale")._2.headOption.flatMap(_.headOption).filterNot(_ == null).map(_.toLong)
+  }
+  
+  def findByOrderId(orderId: Long) = {
+    this.find(By(Sale.orderId, orderId))
   }
 }
