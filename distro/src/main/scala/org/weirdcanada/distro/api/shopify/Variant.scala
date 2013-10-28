b5073be4ba180ef9cd926c379aa5d97c5f373299
PhilipDavis <mrphilipadavis@gmail.com>
2013-10-27 21:34:20 -0400

    Only include non-blank fields
    when serializing a product variant

--- distro/src/main/scala/org/weirdcanada/distro/api/shopify/Variant.scala
+++ distro/src/main/scala/org/weirdcanada/distro/api/shopify/Variant.scala
@@ -91,16 +91,23 @@ class Variant(
   def toJson = compactRender(toJValue).toString
   
   def toJValue: JValue =
-    JObject(List(
-      JField("barcode", JString(barcode)),
-      JField("option1", JString(options.getOrElse(1, ""))),
-      JField("option2", JString(options.getOrElse(2, ""))),
-      JField("option3", JString(options.getOrElse(3, ""))),
-      JField("position", JInt(position)),
-      JField("price", JString(price.toString)),
-      JField("sku", JString(sku)),
-      JField("title", JString(title))
-    ))
+    JObject(
+      List(
+        "barcode" -> barcode,
+        "option1" -> options.getOrElse(1, ""),
+        "option2" -> options.getOrElse(2, ""),
+        "option3" -> options.getOrElse(3, ""),
+        "sku" -> sku,
+        "title" -> title
+      ).collect{
+        case (field, value) if value.length > 0 =>
+          JField(field, JString(value))
+      } :::
+      List(
+        JField("position", JInt(position)),
+        JField("price", JString(price.toString))
+      )
+    )
 }
 
 class PersistentVariant(
