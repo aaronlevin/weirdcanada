265cebdbbdc70a5469e4a9a25bc53e44e691c0da
Aaron Levin <aaron@weirdcanada.com>
2014-01-12 00:45:07 -0500

    Make province selectable on registration form

--- distro/src/main/scala/org/weirdcanada/distro/page/RegisterPage.scala
+++ distro/src/main/scala/org/weirdcanada/distro/page/RegisterPage.scala
@@ -2,17 +2,17 @@ package org.weirdcanada.distro.page
 
 import net.liftweb.http.{DispatchSnippet, SHtml}
 import net.liftweb.util.Helpers._
-import org.weirdcanada.distro.service.Service
 import net.liftweb.http.js._
 import net.liftweb.http.js.JsCmds.FocusOnLoad
-import org.weirdcanada.distro.data.Account
 import net.liftweb.util.Props
-import org.weirdcanada.distro.data.UserRole
 import net.liftweb.http.S
 import scala.xml.Text
 import net.liftweb.common.{Full, Failure}
 import net.liftweb.http.provider.HTTPCookie
 import net.liftweb.common.Empty
+import org.weirdcanada.common.util.Province
+import org.weirdcanada.distro.data.{Account, UserRole}
+import org.weirdcanada.distro.service.Service
 
 class RegisterPage(service: Service) extends DistroPage {
   var emailAddress = ""
@@ -28,13 +28,33 @@ class RegisterPage(service: Service) extends DistroPage {
   var country = ""
   var phoneNumber = ""
   var paypalEmail = ""
+  var usesPaypal = false
     
   def updateEmailAddress(newValue: String, fnAssign: String => Unit) = {
     // TODO: perform basic validation (e.g. matches regex?, no domain name misspellings?, already registered,? etc)
     fnAssign(newValue)
     JsCmds.Noop
   }
-  
+
+  private val paypalToggle: Boolean => JsCmd = b => {
+    usesPaypal = b
+    if(b)
+      JsCmds.Run("""document.getElementById("paypal-email-input").removeAttribute("disabled");""")
+    else
+      JsCmds.Run("""document.getElementById("paypal-email-input").setAttribute("disabled");""")
+  }
+
+  private val handleProvinceSelect: String => JsCmd = (p) => {
+    Province
+      .getProvinceForSlug(p)
+      .map { p => province = p.slug }
+      match { 
+        case None => province = ""; JsCmds.Noop
+        case _ => JsCmds.Noop
+      }
+  }
+
+
   def render = {
     "@email-address" #> FocusOnLoad(SHtml.ajaxText(emailAddress, updateEmailAddress(_, emailAddress = _), "class" -> "form-control", "placeholder" -> "thomas@soft.org")) &
     "@password" #> SHtml.ajaxText(password, password = _, "type" -> "password") &
@@ -43,12 +63,14 @@ class RegisterPage(service: Service) extends DistroPage {
     "@organization" #> SHtml.ajaxText(organization, organization = _) &
     "@address1" #> SHtml.ajaxText(address1, address1 = _) &
     "@address2" #> SHtml.ajaxText(address2, address2 = _) &
-    "@city" #> SHtml.ajaxText(city, city = _, "type" -> "text", "class" -> "form-control") &
-    "@province" #> SHtml.ajaxText(province, province = _, "type" -> "text", "class" -> "form-control") & // TODO: drop down for Canada & US, text entry for all others?
-    "@postal-code" #> SHtml.ajaxText(postalCode, postalCode = _, "type" -> "text", "class" -> "form-control") & // TODO: validate for US & Canada?
-    "@country" #> SHtml.ajaxText(country, country = _, "type" -> "text", "class" -> "form-control") & // TODO: drop down
-    "@phone-number" #> SHtml.ajaxText(phoneNumber, phoneNumber = _, "type" -> "text", "class" -> "form-control", "placeholder" -> "416-555-5555") &
-    "@paypal-email" #> SHtml.ajaxText(paypalEmail, updateEmailAddress(_, paypalEmail = _), "type" -> "text", "class" -> "form-control") &
+    "@city" #> SHtml.ajaxText(city, city = _) &
+    "@province" #> SHtml.ajaxEditableSelect(Seq(("", "---")) ++ Province.provinceNameTuples, Empty, handleProvinceSelect) &
+    //"@province" #> SHtml.ajaxText(province, province = _) & // TODO: drop down for Canada & US, text entry for all others?
+    "@postal-code" #> SHtml.ajaxText(postalCode, postalCode = _) & // TODO: validate for US & Canada?
+    "@country" #> SHtml.ajaxText(country, country = _) & // TODO: drop down
+    "@phone-number" #> SHtml.ajaxText(phoneNumber, phoneNumber = _) &
+    "@uses-paypal" #> SHtml.ajaxCheckbox(false, paypalToggle) &
+    "@paypal-email" #> SHtml.ajaxText(paypalEmail, updateEmailAddress(_, paypalEmail = _)) &
     "@register" #> SHtml.ajaxButton("Create my account", validateAndCreate _, "type" -> "submit", "class" -> "btn btn-default", "onmouseup" -> "$('.error').remove(); $('.has-error').removeClass('has-error');")
   }
 
@@ -71,17 +93,11 @@ class RegisterPage(service: Service) extends DistroPage {
     Rule("country", "country must not be empty", () => country.length > 0),
     Rule("phone-number", "phone number must not be empty", () => phoneNumber.length >= 10),
     Rule("phone-number", "expecting phone number in ###-###-#### format", () => phoneNumber match { case NorthAmericanPhoneNumber(_, _, _) => true case "" => true case _ => false}),
-    Rule("paypal-email", "Paypal email address is invalid", () => service.AccountManager.isValidEmailAddress(paypalEmail))
+    Rule("paypal-email", "Paypal email address is invalid", () => if(usesPaypal) service.AccountManager.isValidEmailAddress(paypalEmail) else true)
   )
   
   def setError(field: String, message: String) = {
-    /*val runString = """
-      (function() {
-        var d = document.getElementById('%s');
-        d.className = d.className + " has-error";
-      })();""".format(field)*/
     val runString = """var yadda = document.getElementById("%s"); yadda.className = yadda.className + " has-error";""".format(field)
-    //val runString = """$('#%s').addClass('has-error');""".format(field)
     JsCmds.Run(runString) & 
     JsCmds.SetHtml(field + "-error", <span class="help-block error" >{message}</span>)
   }
@@ -102,7 +118,6 @@ class RegisterPage(service: Service) extends DistroPage {
     service.AccountManager.createAccount(emailAddress, password, firstName, lastName, organization, address1, address2, city, province, postalCode, country, phoneNumber, paypalEmail) ?~ "createAccount is Empty"
       match {
         case Full(account) =>
-          println("**** AM I HERE?")
           service.SessionManager.current.logIn(account)
           val yearFromNow = 60 * 60 * 24 * 365
           S.addCookie(HTTPCookie("wcdid", Full(account.wcdid.is), if (Props.devMode) Empty else Full(S.hostName), Full("/"), Full(yearFromNow), Empty, Empty, Full(true)))
