62ef285d3853b64b9d76463e2acc1ac50cb3c208
Aaron Levin <aaron@weirdcanada.com>
2014-01-22 12:14:24 -0500

    Create Publisher form

--- distro/src/main/scala/org/weirdcanada/distro/data/Publisher.scala
+++ distro/src/main/scala/org/weirdcanada/distro/data/Publisher.scala
@@ -1,7 +1,12 @@
 package org.weirdcanada.distro.data
 
 import net.liftweb.common.{Full}
+import net.liftweb.http.js.{JsCmd, JsCmds}
 import net.liftweb.mapper._
+import org.weirdcanada.common.util.{Country, Province}
+import org.weirdcanada.dynamicform.{BasicField, DynamicField, DynamicFormFieldRenderHelpers, HasEmpty,HasFields}
+import scala.xml.NodeSeq
+import scalaz.Lens
 
 class Publisher extends LongKeyedMapper[Publisher] with IdPK with Address with ManyToMany {
   def getSingleton = Publisher
@@ -10,10 +15,22 @@ class Publisher extends LongKeyedMapper[Publisher] with IdPK with Address with M
   object url extends MappedString(this, 256)
   object description extends MappedText(this)
   object imageUrl extends MappedString(this, 256)
+  object social extends MappedText(this)
   
   object albums extends MappedManyToMany(PublishersAlbums, PublishersAlbums.album, PublishersAlbums.publisher, Publisher)
   object artists extends MappedManyToMany(ArtistsPublishers, ArtistsPublishers.artist, ArtistsPublishers.publisher, Publisher)
 }
+
+case class PublisherData(
+  name: String,
+  url: String,
+  description: String,
+  imageUrl: String,
+  social: String,
+  city: String,
+  province: String,
+  country: String
+)
 
 // The companion object to the above Class
 object Publisher extends Publisher with LongKeyedMetaMapper[Publisher] {
@@ -25,4 +42,52 @@ object Publisher extends Publisher with LongKeyedMetaMapper[Publisher] {
 
   def findByPartialName(name: String): List[Publisher] = 
     Publisher.findAll(Cmp(Publisher.name, OprEnum.Like, Full("%" + name + "%"), None, Full("LOWER")))
+
+  /**
+   * Setup lenses for the fields on `PublisherData`.
+   */
+  val publisherNameLens: Lens[PublisherData, String] = Lens.lensu( (p, n) => p.copy(name = n), (p) => p.name )
+  val publisherUrlLens: Lens[PublisherData, String] = Lens.lensu( (p, u) => p.copy(url = u), (p) => p.url )
+  val publisherDescriptionLens: Lens[PublisherData, String] = Lens.lensu( (p,d) =>
+      p.copy(description = d), (p) => p.description)
+  val publisherImageUrlLens: Lens[PublisherData, String] = Lens.lensu( (p,i) =>
+      p.copy(imageUrl = i), (p) => p.imageUrl)
+  val publisherSocialLens: Lens[PublisherData, String] = Lens.lensu( (p, s) =>
+      p.copy(social = s), (p) => p.social)
+  val publisherCityLens: Lens[PublisherData, String] = Lens.lensu( (p, c) => p.copy(city = c), (p) => p.city )
+  val publisherProvinceLens: Lens[PublisherData, String] = Lens.lensu( (p, pr) => p.copy(province = pr), (p) => p.province )
+  val publisherCountryLens: Lens[PublisherData, String] = Lens.lensu( (p,c) =>
+      p.copy(country = c), (p) => p.country)
+
+  implicit object PublisherDataEmpty extends HasEmpty[PublisherData] {
+    val empty: PublisherData = PublisherData("","","","","","","","")
+  }
+
+  import DynamicFormFieldRenderHelpers.{textAreaRender, selectRender}
+
+  private val provinceSelectOptions: Seq[(String, String)] = Province.provinceNameTuples
+  private val countrySelectOptions: Seq[(String, String)] = Country.countryTuples
+
+  private val provinceSelect: String => PublisherData => (String => JsCmd) => (NodeSeq => NodeSeq) =
+    selectRender(publisherProvinceLens.get)("name=publisher-province-input")(provinceSelectOptions) _
+  private val countrySelect: String => PublisherData => (String => JsCmd) => (NodeSeq => NodeSeq) =
+    selectRender(publisherCountryLens.get)("name=publisher-country-input")(countrySelectOptions) _
+
+  private val descriptionTextArea = textAreaRender(publisherDescriptionLens.get)("name=publisher-description-input")("Description") _
+
+  implicit object PublisherDataFields extends HasFields[PublisherData] {
+    val fields: List[DynamicField[PublisherData]] = List(
+      BasicField[PublisherData]("publisher-name", publisherNameLens),
+      BasicField[PublisherData]("publisher-url", publisherUrlLens),
+      BasicField[PublisherData]("publisher-description", publisherDescriptionLens, Some(descriptionTextArea)),
+      BasicField[PublisherData]("publisher-image-url", publisherImageUrlLens),
+      BasicField[PublisherData]("publisher-social", publisherSocialLens),
+      BasicField[PublisherData]("publisher-city", publisherCityLens),
+      BasicField[PublisherData]("publisher-province", publisherProvinceLens, Some(provinceSelect)),
+      BasicField[PublisherData]("publisher-country", publisherCountryLens, Some(countrySelect))
+    )
+  }
+
 }
+
+
