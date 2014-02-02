a02b8edd874c2bb32d1241e45ab4ff22d5c6ff92
PhilipDavis <mrphilipadavis@gmail.com>
2014-02-01 22:40:56 -0500

    Removed dead code
    (this is no longer needed now that we're using the SKU field... don't
    need to add a variant by the metafields anymore)

--- distro/src/main/scala/org/weirdcanada/distro/api/shopify/Shopify.scala
+++ distro/src/main/scala/org/weirdcanada/distro/api/shopify/Shopify.scala
@@ -178,13 +178,6 @@ class Shopify(config: Config) extends Loggable {
     }
   }
 
-  def addProductVariant(productId: Long, metafields: Seq[Metafield], variantOptions: Map[Int,String], options: (String, Any)*): PersistentVariant = {
-    post("/admin/products/%s/variants.json".format(productId), Variant.ByMetafields(metafields, variantOptions)) match {
-      case Variant(result) => result
-      case _ => sys.error("Failed to add product %s variant: %s".format(productId, metafields.toString))
-    }
-  }
-
   
   private val metafieldOptions: (String, Any) =
     "fields" -> "created_at,id,key,value,value_type,namespace"
