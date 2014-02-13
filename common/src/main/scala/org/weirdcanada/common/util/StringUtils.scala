2c6b13185637a841d62bc9eb3fa609dad592e674
aaron levin <vilevin@gmail.com>
2014-02-12 21:52:21 -0500

    capitalization

--- common/src/main/scala/org/weirdcanada/common/util/StringUtils.scala
+++ common/src/main/scala/org/weirdcanada/common/util/StringUtils.scala
@@ -4,4 +4,10 @@ object StringUtils {
 
   def trim(s: String): String = s.dropWhile(_ == ' ').reverse.dropWhile(_ == ' ').reverse.toString
 
+  def capitalizeFirstLetter(s: String) = 
+    if( s.isEmpty)
+      s
+    else
+      Character.toUpperCase(s(0)) + s.substring(1, s.length).toLowerCase
+
 }
